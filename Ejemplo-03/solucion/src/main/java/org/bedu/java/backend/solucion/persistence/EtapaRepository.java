442cc017d41ceeaeee52cb4a5a0c91fafc404688
Beto <49173908+beduExpert@users.noreply.github.com>
2022-04-06 17:46:26 -0500

    Delete Ejemplo-03 directory

--- Ejemplo-03/solucion/src/main/java/org/bedu/java/backend/solucion/persistence/EtapaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.bedu.java.backend.solucion.persistence;
-
-import org.bedu.java.backend.solucion.model.Etapa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EtapaRepository extends JpaRepository<Etapa, Long> {
-
-}
