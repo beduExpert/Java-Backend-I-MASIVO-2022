dc8635ff2ba8d3d5399846920db10c07193ed3e7
Beto <49173908+beduExpert@users.noreply.github.com>
2022-04-06 17:46:10 -0500

    Delete Ejemplo-01 directory

--- Ejemplo-01/solucion/src/main/java/org/bedu/java/backend/solucion/persistence/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.bedu.java.backend.solucion.persistence;
-
-import org.bedu.java.backend.solucion.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository  extends JpaRepository<Cliente, Long> {
-
-}
